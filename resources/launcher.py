af0c916d87f1023834b04e2757287550c79685ab
Buzatel-Iulian <iulian.buzatel@gmail.com>
2021-11-12 09:57:34 +0200

    Code Cleanup

--- resources/launcher.py
+++ resources/launcher.py
@@ -3,98 +3,61 @@ import multiprocessing
 from sys import stdout
 import platform
 
-class launcher2:
-    def __init__(self, command="whoami", is_background=True):
+class launcher:
+    def __init__(self, command="whoami", task_name="", is_background=True):
         self.command = command
-        self.name = command.split()[0]
-        self.logfile = self.name + "_log.txt"
+        self.logfile = command.split()[0] + "log.txt"
         self.aplication = None
-        #self.event = multiprocessing.Event()
+        self.task_name = task_name
         self.is_background = is_background
-        self.pid = None
-        #self.mgr = None
-        #self.mgr = multiprocessing.Manager()
-        #self.p_data = self.mgr.dict()
-        self.p_data = None
-        #self.p_data["pid"]=None
-        #self.p_data["is_background"]=is_background
 
     def Execute(self):
         print('Executing command:\n' + ' '.join(self.command))
 
         process = subprocess.Popen(self.command, stdout=subprocess.PIPE,
                                     stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
-        self.pid = process.pid
-        #data["pid"] = process.pid
-        print (f"PID : {self.pid}")
+        
         if not self.is_background:
             out_line = ''
             while True:
-                #print(6)
+                
                 try:
-                    out_line = process.stdout.readline()      #    <<<   IT GETS STUCK HERE !!!
-                    #print(2)
+                    out_line = process.stdout.readline()
                 except:
                     out_line = '\n'
-                    #print(3)
-                #print(4)
+
                 if out_line == '' and process.poll() is not None:
                     break
-                #print(5)
-                #stdout.write(out_line)
-                #stdout.flush()
+                
                 print(out_line)
-                #if self.event.is_set() :
-                    #self.StopProcess(process)
-                    #break
-                #print(1)
+                
         else :
             log = open(self.logfile, "w")
             out_line = ''
             while True:
-                #print(6)
+                
                 try:
-                    out_line = process.stdout.readline()      #    <<<   IT GETS STUCK HERE !!!
-                    #print(2)
+                    out_line = process.stdout.readline() 
                 except:
                     out_line = '\n'
-                    #print(3)
-                #print(4)
+                    
                 if out_line == '' and process.poll() is not None:
                     break
-                #print(5)
+                
                 log.write(out_line)
-                ##stdout.write(out_line)
-                ##stdout.flush()
-
-                #if self.event.is_set() :
-                    #self.StopProcess(process)
-                    #break
-                #print(1)
             log.close()
-        
-        #return process
 
     def launch(self):
-        self.pid = -1
-        #self.mgr = multiprocessing.Manager()
-        #self.p_data = self.mgr.dict()
-
-        self.aplication=multiprocessing.Process(name=self.name,target=self.Execute)
+        self.aplication=multiprocessing.Process(target=self.Execute)
         self.aplication.start()
 
     def stop(self):
-        #self.event.set()
         self.aplication.terminate()
         self.aplication.join()
-
-        print(self.pid)
-        #print(p_data["pid"])
         if platform.system() == "Windows":
-            Execute("taskkill /F /IM node.exe", is_background = False)
+            Execute("taskkill /F /IM {}.exe".format(self.task_name), is_background = False)
         else:
-            Execute("killall node", is_background = False)
-
+            Execute("killall {}".format(self.task_name), is_background = False)
 
 
 def Execute(command_list, is_background=False):
@@ -115,67 +78,4 @@ def Execute(command_list, is_background=False):
       stdout.write(out_line)
       stdout.flush()
   return process
-
-class launcher:
-
-    def __init__(self, server_port="4723", server_ip="127.0.0.1", device_port="5554", device="Pixel_3_API_27"):
-        #self.appium_command = "appium -p 4723 -a 127.0.0.1 -pa /wb/hub -bp 5554 --allow-cors -g appium_log1.txt"
-        self.appium_command = f"appium -p {server_port} -a {server_ip} -pa /wb/hub -bp {device_port} --allow-cors -g appium_log.txt"
-        self.emulator_command = f"emulator -avd {device}"
-        self.appium = None
-        self.emulator = None
-
-    def Execute(self, command, is_background=False):
-  
-        print('Executing command:\n' + ' '.join(command))
-
-        process = subprocess.Popen(command, stdout=subprocess.PIPE,
-                                    stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
-        if not is_background:
-            out_line = ''
-            while True:
-                print(6)
-                try:
-                    out_line = process.stdout.readline()      #    <<<   IT GETS STUCK HERE !!!
-                    print(2)
-                except:
-                    out_line = '\n'
-                    print(3)
-                print(4)
-                if out_line == '' and process.poll() is not None:
-                    break
-                print(5)
-                stdout.write(out_line)
-                stdout.flush()
-                print(1)
-        
-        return process
-
-    def StopProcess (self, process):
-        if process is not None:
-            process.terminate()
-            try:
-                process.wait(3)
-            except subprocess.TimeoutExpired:
-                process.kill()
-
-    def startEmulator (self):
-        print("Starting Emulator")
-        self.emulator = self.Execute(self.emulator_command, is_background = False)
-        if self.emulator.stdout.readline() and self.emulator.poll() is None:
-            print("Emulator is running\n")
-
-    def startAppium (self):
-        print("Starting Appium Server")
-        self.appium = self.Execute(self.appium_command, is_background = True)    #  <<< NEEDS TO GET OUTPUT REDIRECTED
-        if self.appium.stdout.readline() and self.appium.poll() is None:
-            print("Appium is running\n")
-
-    def stopEmulator (self):
-        print("Turning Emulator Off")
-        self.StopProcess(self.emulator)
-
-    def stopAppium (self):
-        print("Turning Appium Server Off")
-        self.StopProcess(self.appium)
 
--- tasks.py
+++ tasks.py
@@ -1,26 +1,10 @@
 import invoke
-import resources.launcher as launcher
+import resources.launcher as lutil
 
-@invoke.task()
-def emulator_test(c):
-    launch = launcher.launcher()
-    launch.startEmulator()
-    launch.startAppium()
-    launch.Execute("robot -d ./output tests/app_test.robot", is_background = False)
-    launch.stopAppium()
-    launch.stopEmulator()
-
-@invoke.task()
-def device_test(c):
-    launch = launcher.launcher(device="ZH800695RV")
-    
-    launch.startAppium()
-    launch.Execute("robot -d ./output -v PLATFORM_VERSION:5.1.1 -v DEVICE_NAME:ZH800695RV tests/app_test.robot", is_background = False)
-    launch.stopAppium()
     
 @invoke.task()
 def launch_test(c):
-    appium = launcher.launcher2(command = "appium -p 4723 -a 127.0.0.1 -pa /wb/hub -bp 5554 --allow-cors -g appium_log1.txt")
+    appium = lutil.launcher(command = "appium -p 4723 -a 127.0.0.1 -pa /wb/hub -bp 5554 --allow-cors -g appiumlog1.txt", task_name = "node")
     appium.launch()
-    robot = launcher.Execute("robot -d ./output -v PLATFORM_VERSION:5.1.1 -v DEVICE_NAME:ZH800695RV tests/app_test.robot", is_background = False)
+    lutil.Execute("robot -d ./output -v PLATFORM_VERSION:5.1.1 -v DEVICE_NAME:ZH800695RV tests/app_test.robot", is_background = False)
     appium.stop()
